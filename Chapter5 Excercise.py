ce7f9d85b3983f486c4ec82ced09ee24a2fe8a59
NeulboGom <jinhee930409@inha.edu>
2023-01-13 19:27:52 +0900

    Chapter5 Excercise2

--- Chapter5 Excercise.py
+++ Chapter5 Excercise.py
@@ -1,61 +1,51 @@
 #Chpter5 Excercise
 
-#5.1 m으로 시작하는 단어를 대문자로 만들어보자.
-song="""When an eel grabs your arm,
-And it causes great harm,
-Tha's - a moray!"""
-
-simple_song=song.strip("!")
-list_song=simple_song.split()
-
-for word in list_song:
-    if word.startswith("m"):
-        capital_word=word.capitalize()
-        print(capital_word)
-list_song[-1]=capital_word
-print(list_song)
-
-for i in list_song:
-    remake_song=" ".join(list_song)
-print(remake_song)
-
-
-#5.2
-question=["We don't serve strings around here. Are you a string?",
-          "What is said on Father's Day in the forest?",
-          "What makes the sound 'Sis! Boom! Bah!'?"]
-answers=["An exploding sheep.",
-         "No, I'm a frayed knot.",
-         "'Pop!' goes the weasel."]
-for i in range(0,3):
-    print(f"Q:{question[i]}\nA:{answers[i]}\n")
-
-#5.3
-print('''My kitty cat likes %s
-My kitty cat likes %s
-My kitty cat fell on his %s And now thinks he's a %s''' %("roast beef", "ham","head","clam"))
-
 #5.4
-letter='''Dear {salutation} {name},
-        Thank you for your letter. We are sorry that our {product} {verbed} in your
-    {room}. Please note that it should never be used in a {room}, especially near any
-    {animals}.
+letter='''Dear {} {},
+        Thank you for your letter. We are sorry that our {} {} in your
+    {}. Please note that it should never be used in a {}, especially near any
+    {}.
         
-        Send us your receipt and {amount} for shipping and handling. We will send you
-    another {product} that, in our tests, is {percent}% less likely to have {verbed}.
+        Send us your receipt and {} for shipping and handling. We will send you
+    another {} that, in our tests, is {}% less likely to have {}.
     
         Thank you for your support.
         Sincerely,
-        {spokeman}
-        {job_title}'''
+        {}
+        {}'''
 salutation="nice"
 name="Kim"
 product="Assult Riple"
 verbed="used"
 room="bedroom"
+animals="dogs"
 amount="the amount"
 percent="5"
 spokeman="Mr.Rho"
 job_title="Samsung"
 
-print(letter.format(salutation, name, product, verbed, room, amount, percent, spokeman, job_title))
+print(letter.format(salutation, name, product, verbed, room, room, animals, amount, product, percent, verbed, spokeman, job_title))
+
+
+#5.6
+
+first="duck"
+second="gourd"
+third="spitz"
+cap_first=first.capitalize()
+cap_second=second.capitalize()
+cap_third=third.capitalize()
+
+print('''%sy Mc%sface
+%sy Mc%sface
+%sy Mc%sface''' % (cap_first, cap_first, cap_second, cap_second, cap_third, cap_third))
+
+#5.7
+print('''{}y Mc{}face
+{}y Mc{}face
+{}y Mc{}face'''.format(cap_first, cap_first, cap_second, cap_second, cap_third, cap_third))
+
+print(f'''{cap_first}y Mc{cap_first}face
+{cap_second}y Mc{cap_second}face
+{cap_third}y Mc{cap_third}face''')
+

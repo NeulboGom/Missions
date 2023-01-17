cb4783b60bcf605dce149e6d8b52e045c54573c2
NeulboGom <jinhee930409@inha.edu>
2023-01-17 17:25:59 +0900

    Chapter8 Excercise2 - Set and Dictionary part

--- Chapter8 Excercise.py
+++ Chapter8 Excercise.py
@@ -1,41 +1,30 @@
 #Chapter8 Excercise
 
-#8.1 영어-프랑스어 사전을 의미하는 e2f 딕셔너리를 만들어 출력해보자. 영어 dog는 프랑스어 chien이고, cat은 chat, walrus는 morse다.
-e2f={"dog":"chien","cat":"chat","walrus":"morse"}
+#8.11 셋 컴프리헨션을 이용하여 range(10)에서 홀수 셋을 만든다.
+odd_set={number for number in range(10) if number%2!=0}
+print(odd_set)
 
-#8.2 e2f 딕셔너리에서 영어 walrus를 프랑스어로 출력해보자
-print(e2f["walrus"])
+#8.12 제너레이터 컴프리헨션을 이용하여 문자열 'Got'과 range10의 각 숫자를 반환한다.
+#     for 문을 사용해서 제너레이터를 순회한다.
+gen_=(n for n in range(10))
+for v in gen_:
+    print("Got",v)
 
-#8.3 e2f 딕셔너리에서 f2e 딕셔너리라는 영어-프랑스어 사전을 만들어보자(items method 사용)
-f2e={}
-for key,value in e2f.items():
-    f2e[value]=key
-print(f2e)
+#8.13 zip()을 사용해서 딕셔너리를 생성한다. key로 ('optimist','pessimist','troll') 튜플을 사용하고
+#     값으로 ('The glass is half full','The glass is half emplty','How did you get a glass?') 튜플을 사용한다
+tuple_keys=('optimist', 'pessimist', 'troll')
+tuple_values=('The glass is half full',
+              'The glass is half empty',
+              'How did you get a glass?')
+answer_dict={}
+for key,value in zip(tuple_keys, tuple_values):
+    answer_dict[key]=value
+print(answer_dict)
 
-#8.4 ef2딕셔너리를 사용해서 프랑스어 chien을 영어로 출력해보자
-print(f2e["chien"])
-
-#8.5 e2f 딕셔너리의 영어 단어 키들을 출력해보자
-print(e2f.keys())
-
-#8.6 이차원 딕셔너리 life를 만들어보자. 최상위 키는 'animals','plants','other'다.
-#    그리고 'animals'는 각각 'cats', 'octopi','emus'를 키로 하고,
-#    'Henri','Grumpy','Lucy'를 값으로 하는 또 다른 딕셔너리를 참조하고 있다
-#    나머지 요소는 빈 딕셔너리를 참조한다
-
-life={"animals": {"cats":"Henri","octopi":"Grumpy","emus":"Lucy"},
-      "plants":{},
-      "others":{}}
-
-#8.7 life 딕셔너리의 최상위 키를 출력해보자
-print(life.keys())
-
-#8.8 life['animals']의 모든 키를 출력해보자
-print(life['animals'].keys())
-
-#8.9 life['animals]['cats']의 모든 값을 출력해보자
-print(life['animals'].get('cats'))
-
-#8.10 딕셔너리 컴프리헨션으로 squares 딕셔너리를 생성한다. range(10)를 키로 하고 각 키의 제곱을 값으로 한다.
-squares={num:num**2 for num in range(10)}
-print(squares)
+#8.14  zip()을 사용해서 다음 두 리스트를 짝으로 하는 movies 딕셔너리를 만들어보자
+titles=['Creature of Habit', 'Crewel Fate']
+plots=['A nun turns into a mon ster', 'A haunted yarn shop']
+required_dict={}
+for t,p in zip(titles,plots):
+    required_dict[t]=p
+print(required_dict)

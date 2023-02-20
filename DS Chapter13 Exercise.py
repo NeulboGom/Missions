dc855f5fdd68bb29b3e115bb7a18158f3a9ac652
NuelboGom <jinhee930409@inha.edu>
2023-02-20 17:47:58 +0900

    DS Chapter13 Exercise02

--- DS Chapter13 Exercise.py
+++ DS Chapter13 Exercise.py
@@ -1,42 +1,53 @@
-# Chapter13 Exercise01 편의점에서 판매되는 물건 목록과 개수 세기
+# Chapter13 Exercise02 순차 검색과 이진 검색의 성능 비교하기
 
 import random
 
-sold_list = ['코카콜라', '츄파춥스', '도시락', '삼각김밥', '레쓰비캔커피', '바나나맛 우유']
-today_list_notsorted = [random.choice(sold_list) for _ in range(20)]
-print(f"# 오늘 판매된 전체 물건(중복O, 정렬X) --> {today_list_notsorted}")
-
-today_list_sorted = sorted(today_list_notsorted)
-print(f"# 오늘 판매된 전체 물건(중복O, 정렬O) --> {today_list_sorted}")
-
-today_list = list(set(today_list_notsorted))
-print(f"# 오늘 판매된 물품 종류(중복X) --> {today_list}")
+num_list_not_sorted = [random.randint(0, 999999) for _ in range(1000000)]
+num_list_sorted = sorted(num_list_not_sorted)
 
 
-def binary_search(ls, searching_data):
+# 순차 검색
+count_sequence = 0
+
+
+def sequence_search(ls, data):
+    global count_sequence
+    idx = -1
+    size = len(ls)
+    for i in range(size):
+        count_sequence += 1
+        if ls[i] == data:
+            idx = i
+            break
+    return idx
+
+
+# 이진 검색
+count_binary = 0
+
+
+def binary_search(ls, data):
+    global count_binary
     start = 0
     end = len(ls) - 1
     while start <= end:
+        count_binary += 1
         mid = (start + end) // 2
-        if ls[mid] == searching_data:
+        if ls[mid] == data:
             return mid
-        elif ls[mid] < searching_data:
+        elif ls[mid] < data:
             start = mid + 1
-        elif ls[mid] > searching_data:
+        else:
             end = mid - 1
 
     return -1
 
 
-final_sell_list = []
-for stuff in today_list:
-    position = 0
-    count = 0
-    while position != -1:
-        position = binary_search(today_list_notsorted, stuff)
-        if position != -1:
-            count += 1
-            del(today_list_notsorted[position])
-    final_sell_list.append((stuff, count))
+print(f"# 비정렬 배열(100만개) -> {num_list_not_sorted}")
+print(f"# 정렬 배열(100만개) -> {num_list_sorted}")
 
-print(f"결산 결과 --> {final_sell_list}")
+f_data = random.choice(num_list_not_sorted)
+sequence_search(num_list_not_sorted, f_data)
+print(f"순차 검색(비정렬 데이터) --> {count_sequence}회")
+binary_search(num_list_sorted, f_data)
+print(f"이진 검색(정렬 데이터) --> {count_binary}회")
